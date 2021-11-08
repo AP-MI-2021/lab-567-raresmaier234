0cba19e30d9dd517b0c43567bbbf047f6f169792
raresmaier234 <raresmaier123@gmail.com>
2021-11-08 19:31:37 +0200

    teste undo/redo

--- Tests/testUndoRedo.py
+++ Tests/testUndoRedo.py
@@ -6,9 +6,90 @@ def test_UndoRedo():
     undo_list = []
     redo_list = []
 
-    rez = addObject("1", "masina", "jucarie", 230, "cora", lista)
-    undo_list.append(rez)
-    lista = rez
-
+    lista = addObject("11", "masina", "jucarie", 230, "cora", lista)
+    undo_list.append(lista)
+    lista = addObject("22", "carte", "bucate", 50, "lidl", lista)
+    undo_list.append(lista)
+    lista = addObject("33", "carti", "joc", 25, "toys", lista)
+    undo_list.append(lista)
+    assert len(undo_list) == 3
+    undo_list.pop()
+    redo_list.append(lista)
+    assert len(undo_list) == 2
+    undo_list.pop()
+    redo_list.append(lista)
     assert len(undo_list) == 1
+    undo_list.pop()
+    redo_list.append(lista)
+    assert len(undo_list) == 0
+    assert len(redo_list) == 3
+
+    redo_list = []
+    lista = addObject("1", "masina", "jucarie", 230, "cora", lista)
+    undo_list.append(lista)
+    redo_list.clear()
+    lista = addObject("2", "carte", "bucate", 50, "lidl", lista)
+    undo_list.append(lista)
+    redo_list.clear()
+    lista = addObject("3", "carti", "joc", 25, "toys", lista)
+    undo_list.append(lista)
+    redo_list.clear()
     assert len(redo_list) == 0
+    assert len(undo_list) == 3
+    undo_list.pop()
+    redo_list.append(lista)
+    assert len(redo_list) == 1
+    assert len(undo_list) == 2
+    undo_list.pop()
+    redo_list.append(lista)
+    assert len(redo_list) == 2
+    assert len(undo_list) == 1
+
+    redo_list.pop()
+    undo_list.append(lista)
+    assert len(redo_list) == 1
+    assert len(undo_list) == 2
+
+    redo_list.pop()
+    undo_list.append(lista)
+    assert len(redo_list) == 0
+    assert len(undo_list) == 3
+
+    undo_list.pop()
+    redo_list.append(lista)
+    assert len(redo_list) == 1
+    assert len(undo_list) == 2
+
+    lista = addObject("4", "masina", "jucarie", 33, "toys", lista)
+    undo_list.append(lista)
+    redo_list.clear()
+    assert len(redo_list) == 0
+    assert len(undo_list) == 3
+
+    undo_list.pop()
+    redo_list.append(lista)
+    assert len(undo_list) == 2
+    assert len(redo_list) == 1
+
+    undo_list.pop()
+    redo_list.append(lista)
+    assert len(undo_list) == 1
+    assert len(redo_list) == 2
+
+    redo_list.pop()
+    undo_list.append(lista)
+    assert len(undo_list) == 2
+    assert len(redo_list) == 1
+
+    redo_list.pop()
+    undo_list.append(lista)
+    assert len(undo_list) == 3
+    assert len(redo_list) == 0
+
+    if len(redo_list) > 0:
+        redo_list.pop()
+    assert len(redo_list) == 0
+
+
+
+
